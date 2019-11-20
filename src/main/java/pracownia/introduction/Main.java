0ca02a010b7db8133acd3f345718d840018c2750
rvz0r <szymon.komosinski98@gmail.com>
2019-11-20 13:29:24 +0100

    first exercises

--- src/main/java/pracownia/introduction/Main.java
+++ src/main/java/pracownia/introduction/Main.java
@@ -1,8 +1,10 @@
 package pracownia.introduction;
+import org.apache.log4j.Logger;
 
 public class Main {
-
+    final static Logger log = Logger.getLogger(Main.class);
     public static void main(String [ ] args) {
         System.out.println("Hello World");
+        log.info("message");
     }
 }
